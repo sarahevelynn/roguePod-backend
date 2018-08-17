17ad0e70f06318f13c454ed6ad3c00f939ecafe5
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-08-17 13:44:39 -0600

    updated content format

--- database/index.js
+++ database/index.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-const uri = 'mongodb://localhost:27017/roguepod-backend'
+const uri = "mongodb://localhost:27017/roguepod-backend";
 
 mongoose.connect(uri, { useNewUrlParser: true }).then(
-    () => {
-        console.log('Connected to Mongo');
+  () => {
+    console.log("Connected to Mongo");
+  },
+  err => {
+    console.log("error connecting to Mongo: ");
+    console.log(err);
+  }
+);
 
-    },
-    err => {
-         console.log('error connecting to Mongo: ')
-         console.log(err);
-
-        }
-  );
-
-
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
